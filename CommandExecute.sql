48a1b456d60c0fc0d59663359344c951e96816a9
mpaycer <michael.paycer@gmail.com>
2026-06-06 07:05:40 -0500

    Test

--- CommandExecute.sql
+++ CommandExecute.sql
@@ -1,5 +1,7 @@
 ﻿SET ANSI_NULLS ON
 GO
+
+--test
 SET QUOTED_IDENTIFIER ON
 GO
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CommandExecute]') AND type in (N'P', N'PC'))
